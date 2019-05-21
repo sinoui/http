7bf1975aee79cac3bf830547226deb2b4ac9f757
刘进行 <jackingstarliu@gmail.com>
2019-05-21 19:25:04 +0800

    chore: 100%单元测试覆盖率

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -9,7 +9,13 @@ afterEach(() => {
   mockAxios.reset();
 });
 
-it('http.get ha been called', async () => {
+it('http拥有四个基本方法', () => {
+  ['get', 'put', 'post', 'delete'].forEach((methodName) =>
+    expect(http).toHaveProperty(methodName),
+  );
+});
+
+it('发送get请求，并获取到响应数据', async () => {
   const promise = http.get('/test');
 
   expect(mockAxios.get).toHaveBeenCalled();
@@ -60,4 +66,49 @@ it('取消监听', async () => {
   await promise;
 
   expect(callback).not.toHaveBeenCalled();
+});
+
+it('500响应', async () => {
+  expect.assertions(3);
+  const errorCallback = jest.fn();
+
+  registerInterceptFunction(errorCallback);
+
+  try {
+    const promise = http.get('/test');
+
+    expect(mockAxios.get).toHaveBeenCalled();
+
+    mockAxios.mockResponse({
+      status: 500,
+      data: 'error stack',
+    });
+
+    await promise;
+  } catch (e) {
+    expect(e.status).toBe(500);
+  }
+
+  expect(errorCallback).toBeCalled();
+});
+
+it('请求发送失败', async () => {
+  expect.assertions(3);
+  const errorCallback = jest.fn();
+
+  registerInterceptFunction(errorCallback);
+
+  try {
+    const promise = http.get('/test');
+
+    expect(mockAxios.get).toHaveBeenCalled();
+
+    mockAxios.mockError(new Error('未知错误'));
+
+    await promise;
+  } catch (e) {
+    expect(e).toBeDefined();
+  }
+
+  expect(errorCallback).toBeCalled();
 });
