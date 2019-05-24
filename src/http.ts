4b753a22958241cec563b5ee33b58017860cac5d
tianyanqiu <tianyanqiu@sinosoft.com.cn>
2019-05-24 11:23:03 +0800

    fix(http)：调整http类型声明

--- src/http.ts
+++ src/http.ts
@@ -1,6 +1,37 @@
-import Axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
+import Axios, { AxiosResponse, AxiosRequestConfig, AxiosInstance } from 'axios';
 
-const http = Axios.create();
+export interface HttpInterface extends AxiosInstance {
+  /**
+   * 获取
+   *
+   * @memberof HttpInterface
+   */
+  get: <T>(url: string, config?: AxiosRequestConfig) => Promise<T>;
+  /**
+   * 删除
+   *
+   * @memberof HttpInterface
+   */
+  delete: <T>(url: string, config?: AxiosRequestConfig) => Promise<T>;
+  /**
+   * post，用于新增
+   *
+   *
+   * @memberof HttpInterface
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  post: <T>(url: string, data?: any, config?: AxiosRequestConfig) => Promise<T>;
+  /**
+   * put，用于更新
+   *
+   *
+   * @memberof HttpInterface
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  put: <T>(url: string, data?: any, config?: AxiosRequestConfig) => Promise<T>;
+}
+
+const http = Axios.create() as HttpInterface;
 
 // 处理响应码
 export const transformResponse = (response: AxiosResponse) => {
